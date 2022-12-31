3a3a16bd0489d9accda30a9ecb1d697f7b6fc81e
smarkwal <stephan@markwalder.net>
2022-12-31 13:48:49 +0100

    Bump Mockito from 4.10.0 to 4.11.0.

--- vtestmail-core/build.gradle.kts
+++ vtestmail-core/build.gradle.kts
@@ -68,7 +68,7 @@ repositories {
 dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    testImplementation("org.mockito:mockito-core:4.10.0")
+    testImplementation("org.mockito:mockito-core:4.11.0")
     testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("com.sun.mail:jakarta.mail:2.0.1")
     testImplementation("commons-net:commons-net:3.9.0")
