4f9c1a0f5c85f3e41b3d84321325f7d30f8cb17b
smarkwal <stephan@markwalder.net>
2023-02-05 13:27:56 +0100

    Bump Mockito to 5.1.1 and Gradle Versions plugin to 0.45.0.

--- vtestmail-core/build.gradle.kts
+++ vtestmail-core/build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 
     // Gradle Versions Plugin
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.44.0"
+    id("com.github.ben-manes.versions") version "0.45.0"
 
     // JarHC Gradle plugin
     id("org.jarhc") version "1.0.1"
@@ -65,7 +65,7 @@ repositories {
 dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    testImplementation("org.mockito:mockito-core:5.0.0")
+    testImplementation("org.mockito:mockito-core:5.1.1")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("com.sun.mail:jakarta.mail:2.0.1")
     testImplementation("commons-net:commons-net:3.9.0")
