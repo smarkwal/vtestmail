ce85c5a679f0bc2745c9f2d21da76f787ef95841
smarkwal <stephan@markwalder.net>
2023-07-31 15:43:18 +0200

    Build: Update Gradle build script for compatibility with Gradle 9. [skip ci]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
 
     // publish to Sonatype OSSRH and release to Maven Central
@@ -7,7 +6,7 @@ plugins {
 }
 
 nexusPublishing {
-    repositories {
+    this.repositories {
         sonatype {
             nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
