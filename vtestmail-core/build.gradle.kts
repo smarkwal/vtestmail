1921754f8a6a41a093e4c0d7f5b9edeb932bb8a3
smarkwal <stephan@markwalder.net>
2023-11-06 17:00:13 +0100

    Bump dependencies.

--- vtestmail-core/build.gradle.kts
+++ vtestmail-core/build.gradle.kts
@@ -9,14 +9,14 @@ plugins {
     `maven-publish`
 
     // run Sonar analysis
-    id("org.sonarqube") version "4.4.0.3356"
+    id("org.sonarqube") version "4.4.1.3373"
 
     // get current Git branch name
-    id("org.ajoberstar.grgit") version "5.2.0"
+    id("org.ajoberstar.grgit") version "5.2.1"
 
     // Gradle Versions Plugin
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.48.0"
+    id("com.github.ben-manes.versions") version "0.49.0"
 
     // JarHC Gradle plugin
     id("org.jarhc") version "1.0.1"
@@ -69,12 +69,12 @@ repositories {
 
 dependencies {
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito:mockito-core:5.5.0")
+    testImplementation("org.mockito:mockito-core:5.7.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("com.sun.mail:jakarta.mail:2.0.1")
-    testImplementation("commons-net:commons-net:3.9.0")
+    testImplementation("commons-net:commons-net:3.10.0")
 
 }
 
