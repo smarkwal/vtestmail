53f4b99621bd2943892e35131979281c704aa165
smarkwal <stephan@markwalder.net>
2023-12-24 13:06:31 +0100

    Bump dependencies.

--- vtestmail-core/build.gradle.kts
+++ vtestmail-core/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
     // Gradle Versions Plugin
     // https://github.com/ben-manes/gradle-versions-plugin
-    id("com.github.ben-manes.versions") version "0.49.0"
+    id("com.github.ben-manes.versions") version "0.50.0"
 
     // JarHC Gradle plugin
     id("org.jarhc") version "1.0.1"
@@ -71,7 +71,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.mockito:mockito-core:5.7.0")
+    testImplementation("org.mockito:mockito-core:5.8.0")
     testImplementation("org.assertj:assertj-core:3.24.2")
     testImplementation("com.sun.mail:jakarta.mail:2.0.1")
     testImplementation("commons-net:commons-net:3.10.0")
